55a177ca9f5d7bb36e0b6b8e8c0c8e8acee5bbd2
Dang Gia Khanh <dagikha0109@gmail.com>
2025-11-22 15:56:02 +0700

    deploy

---
+++ Home.py
@@ -0,0 +1,59 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.markdown("""
+    <style>
+    [data-testid=stSidebarNav] {
+        display:none;
+        }
+    </style>
+""", unsafe_allow_html=True)
+# st.markdown(css,unsafe_allow_html=True)
+
+st.title("Admin Dashboard")
+col1,col2,col3,col4 = st.columns(4)
+
+
+with col1:
+    st.header("Tổng doanh thu")
+    st.metric("Doanh thu","12.5 M","+10%")
+    
+with col2:
+    st.header("Người dùng mới")
+    st.metric("Doanh thu","12.5 M","+10%")
+    
+with col3:
+    st.header("Đơn hàng")
+    st.metric("Doanh thu","327",delta="+11.5%")
+    
+with col4:
+    st.header("Tiền chuyển đổi")
+    st.metric("Doanh thu",value="142",delta="-5%")
+    
+st.markdown("<hr/>",True)
+
+col_char_1,col_char_2 = st.columns(2)
+
+with col_char_1:
+    st.header("Doanh thu 7 ngày gần nhất")
+    st.line_chart([100,90,245,250,430,350,290])
+    
+with col_char_2:
+    st.header("Số lượng đơn theo trạng thái")
+    st.bar_chart([28,90,22,56])
+    
+
+
+#sidebar
+st.sidebar.title("Admin")
+st.sidebar.subheader("Trang 2")
+st.sidebar.markdown("<hr/>",True)
+st.sidebar.title("Menu")
+st.sidebar.page_link("pages/bao_cao.py",label="Report")
+st.sidebar.page_link("pages/nguoi_dung.py",label="User")
+st.sidebar.page_link("pages/bt_menu.py", label="Menu food")
+
+
+
+
+
---
+++ pages/bt_menu.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+# st.set_page_config(layout="wide")
+# st.markdown("""
+#     <style>
+#     [data-testid=stSidebarNav] {
+#         display:none;
+#         }
+#     </style>
+# """, unsafe_allow_html=True)
+
+
+#Wrap menu:
+
+
+#dữ liệu của các món ăn(st.session_state)
+if "lst_mon_an" not in st.session_state:
+    st.session_state.lst_mon_an =[]
+
+st.title("Menu KFC mini")
+
+dict_menu = {
+    "ga_ran":50000,
+    "bo":55000,
+    "pepsi":15000,
+    "kem":20000,
+    "khoai_tay":25000
+}
+
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.header("Chọn Món ăn")
+    frm_mon_an =  st.form("frm_mon_an")
+    with frm_mon_an:
+        # st.title("chọn món ăn")
+        dict_menu["ga_ran"] = st.number_input("Gà rán",min_value=0,step=1) * dict_menu["ga_ran"]
+        dict_menu["bo"] = st.number_input("Bò",min_value=0,step=1) * dict_menu["bo"]
+        dict_menu["khoai_tay"] = st.number_input("Khoai tây chiên",min_value=0,step=1) * dict_menu["khoai_tay"]
+        dict_menu["pepsi"] = st.number_input("Pepsi",min_value=0,step=1) * dict_menu["pepsi"]
+        dict_menu["kem"] = st.number_input("Kem",min_value=0,step = 1) * dict_menu["kem"]
+        
+        btn = frm_mon_an.form_submit_button(label="Đặt món")
+    
+    
+        
+with col2:
+    st.header("Hoá đơn của bạn")
+    st.table(dict_menu)
+    lst_mon_an = []
+    Sum = 0
+    # for i in dict_menu:
+    #     Sum += dict_menu[i]
+    # Sum = f"{Sum:,}"
+    # st.subheader(f"Tổng tiền: {Sum} VND")
+    
+    for i in dict_menu:
+        lst_mon_an.append(dict_menu[i])
+
+
